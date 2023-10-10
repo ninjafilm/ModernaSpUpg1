a6cb33633c9d3766b68ee8b2086aada58553e7fc
Michael Åkerman <michael@ninjafilm.com>
2023-10-11 01:48:11 +0200

    Initial Commit

---
+++ ModernaSpUpg1/ModernaSpUpg1App.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaSpUpg1App.swift
+//  ModernaSpUpg1
+//
+//  Created by Michael Åkerman on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct ModernaSpUpg1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
